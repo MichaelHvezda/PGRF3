f3b860e39fd69763d29c2cec65d6469487f655a6
MichaelHvezda <43959625+MichaelHvezda@users.noreply.github.com>
2020-11-09 18:35:49 +0100

    init

--- src/main/GridFactory.java
+++ src/main/GridFactory.java
@@ -44,4 +44,39 @@ public class GridFactory {
         return new OGLBuffers(vb, attributes, ib);
     }
 
+    public static OGLBuffers generateGridStrip(int a, int b) {
+        float[] vb = new float[a * b * 2];
+        int index = 0;
+
+        for (int j = 0; j < b; j++) {
+            float y = j / (float) (b - 1);
+            for (int i = 0; i < a; i++) {
+//                System.out.println((i / (float) (a - 1) + " " + y));
+                float x = i / (float) (a - 1);
+                vb[index++] = x;
+                vb[index++] = y;
+            }
+        }
+
+        int[] ib = new int[(a - 1) * (b - 1) * 2 * 3];
+        int index2 = 0;
+
+        for (int r = 0; r < b - 1; r++) {
+            int offset = r * a;
+            for (int c = 0; c < a - 1; c++) {
+                ib[index2++] = offset + c;
+                ib[index2++] = offset + c + 1;
+                ib[index2++] = offset + c + a;
+                ib[index2++] = offset + c + 1;
+                ib[index2++] = offset + c + 1 + a;
+                ib[index2++] = offset + c + a;
+            }
+        }
+
+        OGLBuffers.Attrib[] attributes = {
+                new OGLBuffers.Attrib("inPosition", 2) // 2 floats per vertex
+        };
+        return new OGLBuffers(vb, attributes, ib);
+    }
+
 }
